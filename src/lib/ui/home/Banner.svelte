27a2f32a56ea43e85d92fb43f146403d8f5904b1
Viet Trong Duong <ramendev2009@gmail.com>
2023-08-02 07:08:49 +0000

    FEATURE: Adjust padding and add a banner

---
+++ src/lib/ui/home/Banner.svelte
@@ -0,0 +1,22 @@
+<div class="text-center">
+	<h1 class="font-bold rainbow-gradient-text">
+		<span class="banner-big-text">games!</span><br />
+		<span class="text-4xl">that's- that's it i guess</span>
+	</h1>
+	<h2 class="font-semibold text-2xl">
+		bite-sized <span class="rainbow-gradient-text">bits of fun</span> that i will probably bother to
+		update <em>every 200 years</em><br />
+	</h2>
+</div>
+
+<style>
+	.banner-big-text {
+		font-size: 100px;
+	}
+	div {
+		padding: 100px;
+	}
+	h2 {
+		padding: 1rem;
+	}
+</style>
