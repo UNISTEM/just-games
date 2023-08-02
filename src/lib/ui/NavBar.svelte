44f53bc4ee840972214b4e6fa8df1291756c10e5
Ramen Developer <ramendev2009@gmail.com>
2023-08-02 08:31:27 +0700

    PROGRESS: Navbar

---
+++ src/lib/ui/NavBar.svelte
@@ -0,0 +1,17 @@
+<style>
+    button:hover {
+        background-color: orange;
+        color: white;
+        border-radius: 20px;
+        transition: all 0.5s ease;
+    }
+</style>
+
+<div class="text-2xl font-semibold grid-cols-6 font-sans inline-grid w-full h-14">
+    <button>home 🏡</button>
+    <button>games 🎮</button>
+    <button>about 🌈</button>
+    <button>resources 🧰</button>
+    <button>sign in 🚪</button>
+    <button>sign up 🆕</button>
+</div>
