336e5a90f43a3f4f1b6fd2084526ae16e55acfa5
Viet Trong Duong <ramendev2009@gmail.com>
2023-08-02 12:12:14 +0000

    PROGRESS: Fix highlighting for navbar buttons

--- src/lib/ui/home/Banner.svelte
+++ src/lib/ui/home/Banner.svelte
@@ -1,11 +1,11 @@
 <div class="text-center">
-	<h1 class="font-bold rainbow-gradient-text">
+	<h1 class="font-bold rainbow-text">
 		<span class="banner-big-text">games!</span><br />
 		<span class="text-4xl">that's- that's it i guess</span>
 	</h1>
 	<h2 class="font-semibold text-2xl">
-		bite-sized <span class="rainbow-gradient-text">bits of fun</span> that i will probably bother to
-		update <em>every 200 years</em><br />
+		bite-sized <span class="rainbow-text">bits of fun</span> that i will probably bother to update
+		<em>every 200 years</em><br />
 	</h2>
 </div>
 
