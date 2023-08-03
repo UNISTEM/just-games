d059b24585002496becf286734f4733faddce7cc
Ramen Developer <ramendev2009@gmail.com>
2023-08-03 20:26:10 +0700

    FEATURE: Error handling page

--- src/components/home/Banner.svelte
+++ src/components/home/Banner.svelte
@@ -1,9 +1,9 @@
-<div class="text-center">
+<div class="banner text-center">
 	<h1 class="font-bold">
 		<span class="banner-big-text">games!</span><br />
 		<span class="text-4xl">that's- that's it i guess</span>
 	</h1>
-	<h2 class="font-semibold text-2xl">
+	<h2 class="subtitle font-semibold text-2xl">
 		bite-sized <span class="rainbow-text">bits of fun</span> that i will probably bother to update
 		<em>every 200 years</em><br />
 	</h2>
@@ -13,10 +13,10 @@
 	.banner-big-text {
 		font-size: 100px;
 	}
-	div {
-		padding: 100px;
+	.banner {
+		padding: 50px;
 	}
-	h2 {
+	.subtitle {
 		padding: 1rem;
 	}
 </style>
