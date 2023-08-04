3d6bae1c70dffcdf25e4142f5ec89fa77f059dfe
Viet Trong Duong <ramendev2009@gmail.com>
2023-08-04 09:11:57 +0000

    FEATURE: Just a giant Play button in the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+	import GameButton from "../components/home/GameButton.svelte";
 	import HomeBanner from "../components/home/HomeBanner.svelte";
 </script>
 
 <HomeBanner />
+<GameButton />
