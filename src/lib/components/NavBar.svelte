4bd28abbb7a4a7e42cb718fd18751515978f1f39
Ramen Developer <ramendev2009@gmail.com>
2023-08-06 00:19:32 +0700

    FEATURE: Add some semantic HTML elements

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -13,13 +13,15 @@
 </script>
 
 <header class="navbar h-14">
-	<hr class="m-0 mb-5" />
-	<div class="float-left mt-3"><NameLabel /></div>
-	<div class="text-2xl font-semibold grid-cols-6 font-sans inline-grid nobr float-right">
-		{#each buttonData as button}
-			<NavButton link={button[0]} text={button[1]} />
-		{/each}
-	</div>
+	<nav>
+		<hr class="m-0 mb-5" />
+		<div class="float-left mt-3"><NameLabel /></div>
+		<div class="text-2xl font-semibold grid-cols-6 font-sans inline-grid nobr float-right">
+			{#each buttonData as button}
+				<NavButton link={button[0]} text={button[1]} />
+			{/each}
+		</div>
+	</nav>
 </header>
 <br />
 <hr class="-mt-1 m-0 mb-5 ml-[3.2rem] mr-[3.2rem]" />
