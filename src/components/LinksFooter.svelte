8f8e1794f7a58836a2f9acfc229ddeb5dfb4db7a
Viet Trong Duong <ramendev2009@gmail.com>
2023-08-05 06:55:18 +0000

    FEATURE: Take a small swig at Hapara

--- src/components/LinksFooter.svelte
+++ src/components/LinksFooter.svelte
@@ -1,16 +1,16 @@
 <footer>
-<p class="mt-20 gradient-text font-semibold text-center">
+	<p class="mt-20 gradient-text font-semibold text-center">
 		<a href="/">home</a>&nbsp;
 		<a href="/games">games</a>&nbsp;
 		<a href="/about">about</a>&nbsp;
 		<a href="/resources">resources</a>&nbsp;
 		<a href="/login">log in</a>&nbsp;
 		<a href="/signup">sign up</a>
-	<br>
+		<br />
 		<a target="_blank" href="https://github.com/developer-ramen/just-games">github repo</a>&nbsp;
 		<a target="_blank" href="https://github.com/developer-ramen/just-games/issues">report an issue</a>&nbsp;
 		<a target="_blank" href="https://google.com">to-do (trello link, to be set up soon)</a>
-</p>
+	</p>
 </footer>
 
 <style>
