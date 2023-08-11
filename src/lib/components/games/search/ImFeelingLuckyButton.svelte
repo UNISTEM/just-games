786dbe5aa645cbb2021c8e94a96e070f9bd9d4e8
Ramen Developer <ramendev2009@gmail.com>
2023-08-11 20:54:33 +0700

    REFACTOR: Reorganize game search Svelte components

---
+++ src/lib/components/games/search/ImFeelingLuckyButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import lodash from "lodash";
+	import type { GameMetadata } from "$lib/core/game";
+	import { afterNavigate } from "$app/navigation";
+
+	export let searchResults: GameMetadata[];
+	$: randomLink = lodash.sample(searchResults)?.getRouteToGame();
+
+	function generateRandomLink() {
+		randomLink = lodash.sample(searchResults)?.getRouteToGame();
+	}
+	afterNavigate(generateRandomLink);
+</script>
+
+<button class="gradient-text font-semibold text-2xl">
+	<a class="no-underline" href={randomLink}>i'm feeling lucky</a>
+</button>
